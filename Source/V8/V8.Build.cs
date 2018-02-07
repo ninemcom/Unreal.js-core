4579bc0f8c18821022874359b2d816a8aebf8dc4
Jaemin <ppparkje@gmail.com>
2018-02-07 16:18:34 +0900

    switch from V8 to ChakraCore (not buildable)

--- Source/V8/V8.Build.cs
+++ Source/V8/V8.Build.cs
@@ -9,23 +9,23 @@ public class V8 : ModuleRules
         get { return Path.GetFullPath(Path.Combine(ModuleDirectory, "..", "..", "ThirdParty")); }
     }
 
-    public int[] GetV8Version()
+    public int[] GetChakraCoreVersion()
     {
-        string[] VersionHeader = Utils.ReadAllText(Path.Combine(ThirdPartyPath, "v8", "include", "v8-version.h")).Replace("\r\n", "\n").Replace("\t", " ").Split('\n');
+        string[] VersionHeader = Utils.ReadAllText(Path.Combine(ThirdPartyPath, "chakracore", "include", "ChakraCoreVersion.h")).Replace("\r\n", "\n").Replace("\t", " ").Split('\n');
         string VersionMajor = "0";
         string VersionMinor = "0";
         string VersionPatch = "0";
         foreach (string Line in VersionHeader)
         {
-            if (Line.StartsWith("#define V8_MAJOR_VERSION"))
+            if (Line.StartsWith("#define CHAKRA_CORE_MAJOR_VERSION"))
             {
                 VersionMajor = Line.Split(' ')[2];
             }
-            else if (Line.StartsWith("#define V8_MINOR_VERSION "))
+            else if (Line.StartsWith("#define CHAKRA_CORE_MINOR_VERSION "))
             {
                 VersionMinor = Line.Split(' ')[2];
             }
-            else if (Line.StartsWith("#define V8_PATCH_VERSION "))
+            else if (Line.StartsWith("#define CHAKRA_CORE_PATCH_VERSION "))
             {
                 VersionPatch = Line.Split(' ')[2];
             }
@@ -38,7 +38,7 @@ public class V8 : ModuleRules
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
         PrivateIncludePaths.AddRange(new string[]
         {
-            Path.Combine(ThirdPartyPath, "v8", "include"),
+            Path.Combine(ThirdPartyPath, "chakracore", "include"),
             Path.Combine("V8", "Private")
         });
 
@@ -62,7 +62,7 @@ public class V8 : ModuleRules
 
         PrivateDependencyModuleNames.AddRange(new string[] 
         { 
-            "libWebSockets"
+            "libWebSockets",
         });
 
         HackWebSocketIncludeDir(Target);
@@ -106,12 +106,12 @@ public class V8 : ModuleRules
 
     private bool LoadV8(ReadOnlyTargetRules Target)
     {
-        int[] v8_version = GetV8Version();
-        bool ShouldLink_libsampler = !(v8_version[0] == 5 && v8_version[1] < 3);
+        int[] node_version = GetChakraCoreVersion();
+        bool ShouldLink_libsampler = !(node_version[0] == 5 && node_version[1] < 3);
 
         if ((Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Win32))
         {
-            string LibrariesPath = Path.Combine(ThirdPartyPath, "v8", "lib");
+            string LibrariesPath = Path.Combine(ThirdPartyPath, "chakracore", "lib");
 
             if (Target.Platform == UnrealTargetPlatform.Win64)
             {
@@ -122,36 +122,21 @@ public class V8 : ModuleRules
                 LibrariesPath = Path.Combine(LibrariesPath, "Win32");
             }
 
-            if (Target.Configuration == UnrealTargetConfiguration.Debug)
-            {
-                LibrariesPath = Path.Combine(LibrariesPath, "Debug");
-            }
-            else
-            {
-                LibrariesPath = Path.Combine(LibrariesPath, "Release");
-            }
+            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "ChakraCore.lib"));
 
-            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_base_0.lib"));
-            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_base_1.lib"));
-            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_base_2.lib"));
-            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_base_3.lib"));
-            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_libbase.lib"));
-            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_libplatform.lib"));
-            PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_nosnapshot.lib"));
-
-            if (v8_version[0] >= 6)
+            if (node_version[0] >= 6)
             {
-                PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_builtins_setup.lib"));
-                PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_builtins_generators.lib"));
+                //PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_builtins_setup.lib"));
+                //PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_builtins_generators.lib"));
             }
 
             if (ShouldLink_libsampler)
             {
-                PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_libsampler.lib"));
+                //PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "v8_libsampler.lib"));
             }
             
 
-            Definitions.Add(string.Format("WITH_V8=1"));
+            Definitions.Add(string.Format("WITH_CHAKRACORE=1"));
 
             return true;
         }
@@ -168,7 +153,7 @@ public class V8 : ModuleRules
             PublicAdditionalLibraries.Add("v8_libplatform");
             PublicAdditionalLibraries.Add("v8_nosnapshot");
 
-            if (v8_version[0] >= 6)
+            if (node_version[0] >= 6)
             {
                 PublicAdditionalLibraries.Add("v8_builtins_setup");
                 PublicAdditionalLibraries.Add("v8_builtins_generators");                
@@ -179,7 +164,7 @@ public class V8 : ModuleRules
                 PublicAdditionalLibraries.Add("v8_libsampler");
             }
 
-            Definitions.Add(string.Format("WITH_V8=1"));
+            Definitions.Add(string.Format("WITH_CHAKRA_CORE=1"));
 
             return true;
         }
@@ -193,7 +178,7 @@ public class V8 : ModuleRules
             PublicAdditionalLibraries.Add("v8_libplatform");
             PublicAdditionalLibraries.Add("v8_nosnapshot");
 
-            if (v8_version[0] >= 6)
+            if (node_version[0] >= 6)
             {
                 PublicAdditionalLibraries.Add("v8_builtins_setup");
                 PublicAdditionalLibraries.Add("v8_builtins_generators");                
@@ -204,7 +189,7 @@ public class V8 : ModuleRules
                 PublicAdditionalLibraries.Add("v8_libsampler");
             }
 
-            Definitions.Add(string.Format("WITH_V8=1"));
+            Definitions.Add(string.Format("WITH_CHAKRA_CORE=1"));
 
             return true;
         }
@@ -218,7 +203,7 @@ public class V8 : ModuleRules
             PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath,"libv8_libplatform.a"));
             PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath,"libv8_nosnapshot.a"));
 
-            if (v8_version[0] >= 6)
+            if (node_version[0] >= 6)
             {
                 PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "libv8_builtins_setup.a"));
                 PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "libv8_builtins_generators.a"));
@@ -229,11 +214,11 @@ public class V8 : ModuleRules
                 PublicAdditionalLibraries.Add(Path.Combine(LibrariesPath, "libv8_libsampler.a"));
             }
 
-            Definitions.Add(string.Format("WITH_V8=1"));
+            Definitions.Add(string.Format("WITH_CHAKRA_CORE=1"));
 
             return true;
         }
-        Definitions.Add(string.Format("WITH_V8=0"));
+        Definitions.Add(string.Format("WITH_CHAKRA_CORE=0"));
         return false;
     }
 }
